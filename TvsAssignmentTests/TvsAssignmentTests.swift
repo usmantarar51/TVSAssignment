ea0a47ffdd91cffe742fce1188641988b4f06b79
Usman Tarar <usman.tarar@systemsltd.com>
2020-01-13 15:04:30 +0500

    Initial Commit

---
+++ TvsAssignmentTests/TvsAssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  TvsAssignmentTests.swift
+//  TvsAssignmentTests
+//
+//  Created by Usman Tarar on 13/01/2020.
+//  Copyright © 2020 Usman Tarar. All rights reserved.
+//
+
+import XCTest
+@testable import TvsAssignment
+
+class TvsAssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
